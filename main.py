f13176fd43f6ef4b294d5ac4ee355db826fc6c94
carl <160986364+Calc2790@users.noreply.github.com>
2025-02-23 13:31:08 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+
+import discord
+import os
+import requests
+import json
+from discord import Intents
+
+# Set up intents 
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
+
+intents = Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!quotes'):
+        quote = get_quote()
+        await message.channel.send(quote)
+      
+
+client.run(os.getenv('TOKEN'))
